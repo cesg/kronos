7b4c20440af533083be48df7dc43df800da66193
Cristian SG <cesgbla@gmail.com>
2017-08-02 19:17:53 -0400

    Don't override bulma colors.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ let mix = require('laravel-mix')
 
 mix.js('resources/assets/js/app.js', 'public/js')
   .sass('resources/assets/sass/app.scss', 'public/css')
-  .extract(['vue', 'vuex', 'vue-router', 'axios', 'buefy'], 'js/vendor')
+  .extract(['vue', 'vuex', 'vue-router', 'axios', 'buefy', 'date-fns'], 'js/vendor')
